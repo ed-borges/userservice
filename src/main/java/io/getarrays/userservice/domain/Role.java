9692fb120d1d9d375927e67d8e0f10dc92d75d25
Edvander Borges <edvander@live.com>
2022-02-02 15:57:54 +0100

    Adicionando os atritutos e mapeando as entidades

--- src/main/java/io/getarrays/userservice/domain/Role.java
+++ src/main/java/io/getarrays/userservice/domain/Role.java
@@ -1,4 +1,21 @@
 package io.getarrays.userservice.domain;
 
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
 }
--- src/main/java/io/getarrays/userservice/domain/User.java
+++ src/main/java/io/getarrays/userservice/domain/User.java
@@ -1,4 +1,41 @@
 package io.getarrays.userservice.domain;
 
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String username;
+    private String password;
+    @ManyToMany(fetch = FetchType.EAGER)
+    private Collection<Role> roles = new ArrayList<>();
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+
+        User user = (User) o;
+
+        return getId().equals(user.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getId().hashCode();
+    }
 }
