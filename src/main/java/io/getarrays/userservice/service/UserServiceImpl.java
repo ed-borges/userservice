b4b51f0add667b94c7b4fafb634f25456819f101
Edvander Borges <edvander@live.com>
2022-02-02 16:50:02 +0100

    Criando o controller do usuario

--- src/main/java/io/getarrays/userservice/service/UserServiceImpl.java
+++ src/main/java/io/getarrays/userservice/service/UserServiceImpl.java
@@ -25,16 +25,19 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public User saveUser(User user) {
+        log.info("Saving new user {} to the database.", user.getName());
         return this.userRepo.save(user);
     }
 
     @Override
     public Role saveRole(Role role) {
+        log.info("Saving new role {} to the database.", role.getName());
         return roleRepo.save(role);
     }
 
     @Override
     public void addUserToRole(String username, String roleName) {
+        log.info("Adding role {} to user {}", roleName, username);
         User user = getUser(username);
         Role role = roleRepo.findByName(roleName);
 
@@ -43,11 +46,13 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public User getUser(String username) {
+        log.info("Fetching user {}.", username);
         return userRepo.findByUsername(username);
     }
 
     @Override
     public List<User> getUsers() {
+        log.info("Fetching all users");
         return userRepo.findAll();
     }
 }
